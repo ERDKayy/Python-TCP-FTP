59351fe6e2a21aee46af0689da9f843a46f53d93
Eric Robert Kimball <C0431296@intra.camosun.bc.ca>
2017-11-06 15:52:29 -0800

    working on commands

--- client.py
+++ client.py
@@ -1,23 +1,25 @@
 import sys
 import socket
 s = socket.socket()         # Create a socket object
-host = sys.argv[1]
+host = sys.argv[1]          # Hostname to use
 port = int(sys.argv[2])     # Port to be used
-command = sys.argv[3]
-file = sys.argv[4]
+command = sys.argv[3]       # GET, PUT, or DEL
+file = sys.argv[4]          # File path of file to transfer
 
 
 # connect to local machine with port given via args
-s.connect((socket.gethostname(), port))
+s.connect((host, port))
 f = open(file,'rb')
-print('Sending...')
-l = f.read(1024)
-while (l):
+
+if command.upper() == 'PUT':
     print('Sending...')
-    s.sendall(l)
     l = f.read(1024)
-f.close()
-print("Done Sending")
+    while l:
+        print('Sending...')
+        s.sendall(l)
+        l = f.read(1024)
+    f.close()
+print("Done")
 s.shutdown(socket.SHUT_WR)
 print(s.recv(1024))
 s.close()
