4105c1b34d1cf4976b20e27d1a8d3cd73944b167
Eric Kimball <eric-k93@live.com>
2017-11-06 21:52:43 -0800

    working on get, infinite loop in server

--- client.py
+++ client.py
@@ -1,25 +1,28 @@
 import sys
 import socket
 s = socket.socket()         # Create a socket object
-host = sys.argv[1]          # Hostname to use
-port = int(sys.argv[2])     # Port to be used
-command = sys.argv[3]       # GET, PUT, or DEL
-file = sys.argv[4]          # File path of file to transfer
+host = socket.gethostname()          # Hostname to use
+port = 12345     # Port to be used
+command = "GET"       # GET, PUT, or DEL
+file = "tosend.png"      # File path of file to transfer
 
 
 # connect to local machine with port given via args
 s.connect((host, port))
-f = open(file,'rb')
 
-if command.upper() == 'PUT':
-    print('Sending...')
-    l = f.read(1024)
-    while l:
-        print('Sending...')
-        s.sendall(l)
-        l = f.read(1024)
-    f.close()
-print("Done")
+if command == "GET":
+    toSend = str(command + " " + file).encode("utf-8")
+    s.send(toSend)
+    f = open(file, 'wb')
+    print("Receiving...")
+    toRecv = s.recv(1024)
+    while toRecv:
+        print("Receiving...")
+        f.write(toRecv)
+        toRecv = s.recv(1024)
+    toRecv.close()
+    print("Done")
+
 s.shutdown(socket.SHUT_WR)
-print(s.recv(1024))
+print(s.recv(1024), end='')
 s.close()
--- server.py
+++ server.py
@@ -4,17 +4,22 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-f = open('torecv.png','wb')
-s.listen(5)                 # Now wait for client connection.
+
+
+s.listen(0)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
-    print('Got connection from', addr)
-    print("Receiving...")
-    l = c.recv(1024)
-    while (l):
-        print("Receiving...")
-        f.write(l)
-        l = c.recv(1024)
-    f.close()
+    received = c.recv(1024)
+    received = received.decode("utf-8")
+    if "GET" in received.upper():
+        received = received.split()
+        file = received[1]
+        file = open(file, 'rb')
+        toSend = file.read(1024)
+        while file:
+            print("sending...")
+            c.sendall(toSend)
+            toSend = file.read(1024)
+        file.close()
     print("Done Receiving")
     c.close()                # Close the connection
