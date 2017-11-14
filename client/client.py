f13fa8ec06c55c809bda5df1300d485d43a555ae
Eric Kimball <eric-k93@live.com>
2017-11-14 07:29:09 -0800

    working on endian order from server to client

--- client/client.py
+++ client/client.py
@@ -6,6 +6,11 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Hostname to use
 port = int(sys.argv[1])              # Port to be used
 
+# Client protocol messages
+ready = "READY".encode("utf-8")
+ok = "OK".encode("utf-8")
+
+
 # connect to local machine with port given via args
 s.connect((host, port))
 
@@ -13,32 +18,41 @@ s.connect((host, port))
 command = sys.argv[2]            # GET, PUT, or DEL
 file = sys.argv[3]        # File path of file to transfer
 toSend = str(command + " " + file).encode("utf-8")
-s.send(toSend)
+readyCheck = s.recv(1024)
 
-# if Get, open the file requested and write blocks as they are received
-if command == "GET":
-    f = open(file, 'wb')
-    toRecv = s.recv(1024)
-    fileSize = os.stat(file).st_size
-    while toRecv:
-        print("Receiving file %s (%d bytes)" % (file, fileSize))
-        f.write(toRecv)
+if readyCheck.decode("utf-8") == "READY":
+    s.send(toSend)
+    # if Get, open the file requested and write blocks as they are received
+    if command == "GET":
+        f = open(file, 'wb')
+        s.send(ready)
+        fileSize = s.recv(1024)
+        s.send(ok)
         toRecv = s.recv(1024)
-        recvSize = os.stat(toRecv).st_size
-        fileSize = (fileSize + recvSize)
-    print("Done")
-
-if command == "PUT":
-    fileSize = os.stat(file).st_size
-    f = open(file, 'rb')
-    toSend = f.read(1024)
-    while toSend:
-        print("Sending file %s (%d bytes)" % (file, (fileSize / 1024)))
-        s.sendall(toSend)
+        #fileSize = os.stat(file).st_size
+        while toRecv:
+            #print("Receiving file %s (%d bytes)" % (file, fileSize))
+            if toRecv.decode("utf-8") == "DONE":
+                sys.exit("Complete")
+            f.write(toRecv)
+            toRecv = s.recv(1024)
+            #recvSize = os.stat(toRecv).st_size
+            #bytesRemaining = fileSize - recvSize
+            #print("Bytes Remaining: %d" % (bytesRemaining))
+            #fileSize = (fileSize + recvSize)
+        print("Done")
+    # if PUT; open file, read bytes into packets and send
+    if command == "PUT":
+        fileSize = os.stat(file).st_size
+        f = open(file, 'rb')
         toSend = f.read(1024)
-        fileSize = (fileSize - 1024)
-    f.close()
-    print("Done")
+        while toSend:
+            print("Sending file %s (%d bytes)" % (file, (fileSize / 1024)))
+            s.sendall(toSend)
+            toSend = f.read(1024)
+            fileSize = (fileSize - 1024)
+        f.close()
+        print("Done")
 
 s.shutdown(socket.SHUT_WR)
 #print(s.recv(1024), end='')
--- server/server.py
+++ server/server.py
@@ -9,22 +9,38 @@ port = int(sys.argv[1])                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 s.listen(0)                 # Now wait for client connection.
 
+# Protocol messages
+ready = "READY".encode("utf-8")
+ok = "OK".encode("utf-8")
+done = "DONE".encode("utf-8")
+
 while True:
     c, addr = s.accept()     # Establish connection with client.
+    c.send(ready)
     commandFile = c.recv(1024)
     commandFile = commandFile.decode("utf-8")
     commandFile = commandFile.split()
     file = commandFile[1]
+
+
     if commandFile[0] == "GET":
-        file = commandFile[1]
-        file = open(file, 'rb')
-        toSend = file.read(1024)
-        while toSend:
-            print("sending...")
-            c.sendall(toSend)
-            toSend = file.read(1024)
-        file.close()
-        print("Done")
+        c.send(ok)
+        readyCheck = c.recv(1024)
+        if readyCheck.decode("utf-8") == "READY":
+            file = commandFile[1]
+            f = open(file, 'rb')
+            fileSize = os.stat(file).st_size
+            fileSize.to_bytes((fileSize.bit_length() + 7) // 8, 'big') or b'\0'
+            c.send(fileSize)
+            okCheck = c.recv(1024)
+            if okCheck.decode("utf-8") == "OK":
+                toSend = f.read(1024)
+                while toSend:
+                    print("sending...")
+                    c.sendall(toSend)
+                    toSend = f.read(1024)
+                file.close()
+                c.send(done)
 
     if commandFile[0] == "PUT":
         file = open(file, 'wb')
