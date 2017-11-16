e81091c8bbb44da38560b916c20defcb4c978838
Eric Kimball <Eric-k93@live.com>
2017-11-16 14:21:40 -0800

    YEEEEEEEET

--- server/server.py
+++ server/server.py
@@ -36,79 +36,78 @@ while True:
     if verbose == True:
         print("Server connected to client at %s" % (address))
     c.send(ready)
-    time.sleep(1)
     commandFile = c.recv(1024)
-    commandFile = commandFile.decode("utf-8")
-    print(commandFile)
-    if verbose == True:
-        print("Server receiving request: %s" % (commandFile))
-    commandFile = commandFile.split()
-    file = commandFile[1]
-
-
-
-    if commandFile[0] == "GET":
-
-        try:
-            f = open(file, 'rb')
-        except IOError:
-            error = "ERROR: " + file + " does not exist"
-            c.send(error.encode('utf-8'))
-            errorCode = True
-
-        if errorCode == False:
-            c.send(ok)
-            readyCheck = c.recv(1024)
-            if readyCheck.decode("utf-8") == "READY":
-                fileSize = int(os.stat(file).st_size)
-                c.send(fileSize.to_bytes(8, byteorder="big", signed=False))
-                okCheck = c.recv(1024)
-                if okCheck.decode("utf-8") == "OK":
-                    if verbose == True:
-                        print("Server sending %d bytes" % (fileSize))
-                    toSend = f.read(1024)
-                    while toSend:
-                        c.sendall(toSend)
-                        toSend = f.read(1024)
-                    f.close()
-                    c.send(done)
-
-    if commandFile[0] == "PUT":
-        c.send(ok)
-        bytes_remaining = c.recv(1024)
-        bytes_remaining = int.from_bytes(bytes_remaining, byteorder='big', signed=False)
-        original_size = bytes_remaining
-
-        try:
-            f = open(file, 'wb')
-        except IOError:
-            error = "ERROR: unable to create file " + file
-            c.send(error.encode('utf-8'))
-            errorCode = True
-
-
-        if errorCode == False:
-            c.send(ok)
-            toRecv = c.recv(min(bytes_remaining, 1024))
-            if verbose == True:
-                print("Server receiving %d bytes" % (original_size))
-            while toRecv:
-                #print("%d of %d remaining..." % (bytes_remaining, original_size))
-                f.write(toRecv)
-                bytes_remaining = bytes_remaining - len(toRecv)
-                toRecv = c.recv(min(bytes_remaining, 1024))
-            f.close()
-            c.send(done)
-
-
-    if commandFile[0] == "DEL":
+    if commandFile:
+        commandFile = commandFile.decode("utf-8")
+        print(commandFile)
         if verbose == True:
-            print("Server deleting file %s" % (file))
+            print("Server receiving request: %s" % (commandFile))
+        commandFile = commandFile.split()
+        file = commandFile[1]
 
-        try:
-            os.remove(file)
-        except IOError:
-            error = ("ERROR: %s does not exist" % (file))
-            c.send(error.encode('utf-8'))
-        c.send(done)
-    c.close()                # Close the connection
+
+        if commandFile[0] == "GET":
+
+            try:
+                f = open(file, 'rb')
+            except IOError:
+                error = "ERROR: " + file + " does not exist"
+                c.send(error.encode('utf-8'))
+                errorCode = True
+
+            if errorCode == False:
+                c.send(ok)
+                readyCheck = c.recv(1024)
+                if readyCheck.decode("utf-8") == "READY":
+                    fileSize = int(os.stat(file).st_size)
+                    c.send(fileSize.to_bytes(8, byteorder="big", signed=False))
+                    okCheck = c.recv(1024)
+                    if okCheck.decode("utf-8") == "OK":
+                        if verbose == True:
+                            print("Server sending %d bytes" % (fileSize))
+                        toSend = f.read(1024)
+                        while toSend:
+                            c.sendall(toSend)
+                            toSend = f.read(1024)
+                        f.close()
+                        c.send(done)
+
+        if commandFile[0] == "PUT":
+            c.send(ok)
+            bytes_remaining = c.recv(1024)
+            bytes_remaining = int.from_bytes(bytes_remaining, byteorder='big', signed=False)
+            original_size = bytes_remaining
+
+            try:
+                f = open(file, 'wb')
+            except IOError:
+                error = "ERROR: unable to create file " + file
+                c.send(error.encode('utf-8'))
+                errorCode = True
+
+
+            if errorCode == False:
+                c.send(ok)
+                toRecv = c.recv(min(bytes_remaining, 1024))
+                if verbose == True:
+                    print("Server receiving %d bytes" % (original_size))
+                while toRecv:
+                    #print("%d of %d remaining..." % (bytes_remaining, original_size))
+                    f.write(toRecv)
+                    bytes_remaining = bytes_remaining - len(toRecv)
+                    toRecv = c.recv(min(bytes_remaining, 1024))
+                f.close()
+                c.send(done)
+
+
+        if commandFile[0] == "DEL":
+            if verbose == True:
+                print("Server deleting file %s" % (file))
+
+            try:
+                os.remove(file)
+            except IOError:
+                error = ("ERROR: %s does not exist" % (file))
+                c.send(error.encode('utf-8'))
+            c.send(done)
+        c.close()                # Close the connection
